835a18ea4003466e3546fab082d663f5ce9ac498
albersola <albsolac@gmail.com>
2026-03-12 20:47:04 +0100

    fix: default database name

--- src/config.py
+++ src/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     postgres_password: str = "postgres"
     postgres_host: str = "localhost"
     postgres_port: int = 5432
-    postgres_db: str = "secondbrain"
+    postgres_db: str = "pensabot"
 
     # Redis
     redis_host: str = "localhost"
